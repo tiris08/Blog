ce2bd15e427dacc10efd9260dbe19d1909ec65e0
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-03-22 11:20:58 +0200

    basic HTTP Authentication added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  http_basic_authenticate_with name: "taras", password: "0124816320", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "taras", password: "0124816320", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
